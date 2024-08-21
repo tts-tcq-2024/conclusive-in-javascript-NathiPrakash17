0303ddc5131e605c70ca910b680d8ceffe0da924
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-22 01:16:21 +0530

    Update temperatureBreachTests.test.js

--- test/temperatureBreachTests.test.js
+++ test/temperatureBreachTests.test.js
@@ -18,7 +18,7 @@ describe('checkAndAlert Function Tests', () => {
   it('should call sendToController for TOO_HIGH breach', () => {
     alerts.checkAndAlert('TO_CONTROLLER', { coolingType: 'MED_ACTIVE_COOLING' }, 44);
     expect(sendToControllerStub.calledOnce).to.be.true;
-    expect(sendToControllerStub.calledWith('TOO_HIGH')).to.be.true; // Fixed expectation
+    expect(sendToControllerStub.calledWith('TOO_HIGH')).to.be.true;
   });
 
   it('should call sendToEmail for TOO_LOW breach', () => {
