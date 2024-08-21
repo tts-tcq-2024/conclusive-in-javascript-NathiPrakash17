c534ff88cd65c1870ace5e780cc5126f388467ea
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-22 00:49:39 +0530

    Update and rename typewisealert.test.js to temperatureBreachTests.test.js

---
+++ test/temperatureBreachTests.test.js
@@ -0,0 +1,42 @@
+const alerts = require('../src/typewisealert');
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+describe('checkAndAlert Function Tests', () => {
+  let sendToControllerStub;
+  let sendToEmailStub;
+
+  beforeEach(() => {
+    sendToControllerStub = sinon.stub(alerts, 'sendToController');
+    sendToEmailStub = sinon.stub(alerts, 'sendToEmail');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should call sendToController for TOO_HIGH breach', () => {
+    alerts.checkAndAlert('TO_CONTROLLER', { coolingType: 'MED_ACTIVE_COOLING' }, 45);
+    expect(sendToControllerStub.calledOnce).to.be.true;
+    expect(sendToControllerStub.calledWith('TOO_HIGH')).to.be.true;
+  });
+
+  it('should call sendToEmail for TOO_LOW breach', () => {
+    alerts.checkAndAlert('TO_EMAIL', { coolingType: 'PASSIVE_COOLING' }, -210);
+    expect(sendToEmailStub.calledOnce).to.be.true;
+    expect(sendToEmailStub.calledWith('TOO_LOW')).to.be.true;
+  });
+
+  it('should call sendToEmail for NORMAL temperature without issues', () => {
+    alerts.checkAndAlert('TO_EMAIL', { coolingType: 'HI_ACTIVE_COOLING' }, 430);
+    expect(sendToEmailStub.notCalled).to.be.true; 
+  });
+
+  it('should handle unknown alert target without throwing an error', () => {
+    const consoleLogSpy = sinon.spy(console, 'log');
+    alerts.checkAndAlert('UNKNOWN_TARGET', { coolingType: 'HI_ACTIVE_COOLING' }, 510);
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('Unknown alert target')).to.be.true;
+    consoleLogSpy.restore();
+  });
+});
